2fd6b1b62b6687be888eaa619d321d2f7d37ca38
Shuai Zhao <zhaoshua@amazon.co.jp>
2018-03-14 22:39:12 +0900

    Hello World

---
+++ src/main/java/Test/HelloWorld.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(greeting());
+    }
+
+    static String greeting() {
+        return "Hello, world!";
+    }
+}
---
+++ src/test/java/Test/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void greeting() {
+        assertEquals(HelloWorld.greeting(), "Hello, world!");
+    }
+}
