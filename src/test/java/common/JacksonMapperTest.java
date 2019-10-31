8211d02a699bef5ad51de825f94ed272aafec08a
Kamel <kamelzcs@gmail.com>
2019-10-31 23:09:09 +0900

    Add generic transformer

--- src/test/java/common/JacksonMapperTest.java
+++ src/test/java/common/JacksonMapperTest.java
@@ -2,7 +2,8 @@ package common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import lombok.*;
+import lombok.Data;
+import lombok.extern.java.Log;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,6 +11,20 @@ import org.junit.Test;
 class Person {
     private final Object name;
 }
+
+interface InputOut<IN, OUT> {
+    OUT transform(IN in);
+}
+
+@Data
+class IntToString implements InputOut<Integer, String> {
+    private final Integer input;
+    @Override
+    public String transform(Integer integer) {
+        return null;
+    }
+}
+@Log
 public class JacksonMapperTest {
     JsonMapper jsonMapper = new JsonMapper();
     @Test
@@ -18,5 +33,15 @@ public class JacksonMapperTest {
         String personValue = jsonMapper.writeValueAsString(person);
         Person person1 = jsonMapper.readValue(personValue, Person.class);
         Assert.assertEquals(person, person1);
+    }
+
+    @Test
+    public void testGenerics() throws JsonProcessingException {
+        IntToString input = new IntToString(1);
+        String value = jsonMapper.writeValueAsString(input);
+        log.info(value);
+        IntToString input2 = jsonMapper.readValue(value, IntToString.class);
+        Assert.assertEquals(input, input2);
+
     }
 }
